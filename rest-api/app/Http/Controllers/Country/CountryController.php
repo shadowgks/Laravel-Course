d72c0d3a9fc480c9be0fbe293e2ecb1752912191
shadowgks <saad.moumou.coder@gmail.com>
2023-02-15 17:31:17 +0100

    begin rest api

---
+++ rest-api/app/Http/Controllers/Country/CountryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CountryController extends Controller
+{
+    function country()
+    {
+        
+    }
+}
---
+++ rest-api/app/Models/Country/Country.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\Country;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Country extends Model
+{
+    use HasFactory;
+    protected $table = 'country';
+    protected $fillable = [
+        'iso',
+        'name',
+        'dname',
+        'iso3',
+        'position',
+        'numcode',
+        'phonecode',
+        'created',
+        'register_by',
+        'modified',
+        'modified_by',
+        'record_deleted'
+    ];
+}
